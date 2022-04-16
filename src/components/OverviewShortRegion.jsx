85cf8ff475d72169105aa0591ff4d8963622fd26
Serhii Vlokh <tany.tany283@gmail.com>
2022-04-16 16:31:44 +0300

    Idk push

--- src/components/OverviewShortRegion.jsx
+++ src/components/OverviewShortRegion.jsx
@@ -1,31 +1,41 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 
 import regionImg from "../assets/default_region_avatar.png"
 
 function OverviewShortRegion(props) {
-	const pages = [
+	const [currentPage, setCurrentPage] = useState(<Region></Region>)
+
+	const pageNames = [
 		'Region',
 		'Country',
 		'World',
 	]
 
+	const pageComponents = [
+		<Region></Region>,
+		<Country></Country>,
+		<World></World>
+	]
+
+	const onLeft = e => {
+
+	}
+
+	const onRight = e => {
+
+	}
 
   return <div className="overview-short-region-container">
-		<div className="row">
-			<img src={regionImg} alt="region"></img>
-			<div className="title-panel col">
-				<label className="region-name">Черкасська область</label>
-				<label className="region-country">Holy Ukrainian Reich</label>
-			</div>
-		</div>
+		{currentPage}
 		<div className="navigation-container row">
-			<label>{pages[2]}</label>
+			<label onClick={onRight} className="left-label">{pageNames[2]}</label>
 			<div className="navigation-indicators-container row">
 				<div className="point"></div>
 				<div className="point"></div>
 				<div className="point"></div>
 			</div>
-			<label>{pages[1]}</label>
+			<label onClick={onRight} className="right-label">{pageNames[1]}</label>
 		</div>
 	</div>
 }
@@ -37,3 +47,33 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, null)(OverviewShortRegion)
+
+function Region() {
+	return <div className="row">
+		<img src={regionImg} alt="region"></img>
+		<div className="title-panel col">
+			<label className="region-name">Черкасська область</label>
+			<label className="region-country">Holy Ukrainian Reich</label>
+		</div>
+	</div>
+}
+
+function Country() {
+	return <div className="row">
+		<img src={regionImg} alt="region"></img>
+		<div className="title-panel col">
+			<label className="region-name">Черкасська область</label>
+			<label className="region-country">Holy Ukrainian Reich</label>
+		</div>
+	</div>
+}
+
+function World() {
+	return <div className="row">
+		<img src={regionImg} alt="region"></img>
+		<div className="title-panel col">
+			<label className="region-name">Черкасська область</label>
+			<label className="region-country">Holy Ukrainian Reich</label>
+		</div>
+	</div>
+}
