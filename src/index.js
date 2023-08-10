25a3c62b75f908bc82da9ab734337916869cf437
Serhii Vlokh <serhii.vlokh@sigma.software>
2023-08-10 15:24:54 +0300

    Update headers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { rootReducer } from './redux/rootReducer';
 import axios from 'axios';
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 
