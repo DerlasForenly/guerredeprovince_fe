7c09bfcdf3113c2f5c29f99b22ae75306640410d
Serhii Vlokh <serhii.vlokh@sigma.software>
2023-08-28 19:34:45 +0300

    Update cors

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import { Provider } from 'react-redux';
 import { rootReducer } from './redux/rootReducer';
 import axios from 'axios';
 
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.common['Access-Control-Allow-Methods'] = '*';
-axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
-axios.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 
 const store = createStore(rootReducer, compose(
